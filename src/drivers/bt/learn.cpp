80fdbbfa0cba9b7c8a2f22d42c14cf3653228799
Bernhard Wymann <berni4you@gmx.ch>
2004-08-25 14:38:21 +0000

    Added forgotten license headers.

--- src/drivers/bt/learn.cpp
+++ src/drivers/bt/learn.cpp
@@ -1,4 +1,21 @@
+/***************************************************************************
 
+    file                 : learn.cpp
+    created              : Wed Aug 28 16:36:00 CET 2004
+    copyright            : (C) 2004 by Bernhard Wymann
+    email                : berniw@bluewin.ch
+    version              : $Id$
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 
 
 #include "learn.h"
