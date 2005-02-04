bb165db8601128bc6587a5fad6b8c3814439af67
Bernhard Wymann <berni4you@gmx.ch>
2005-02-04 19:43:40 +0000

    Added most important files for loki installer.

---
+++ installer/loki/setup.data/scripts/tools_finalize.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Set up path in torcs startup script.
+
+function updatepath () {
+	fn=${SETUP_INSTALLPATH}$1
+
+	# rip off last "/"
+	ip=${SETUP_INSTALLPATH/%\//}
+
+	# replace all "/" with "\/" to use with regexp.
+	fn2=${ip//\//\\\/}
+
+ed -s $fn <<EOF
+,s/@prefix@/$fn2/g
+,wq
+EOF
+}
+
+for f in accc nfs2ac nfsperf texmapper trackgen;
+do updatepath $f ;
+done
+
---
+++ installer/loki/setup.data/scripts/torcs_finalize.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Set up path in torcs startup script.
+
+fn=${SETUP_INSTALLPATH}torcs
+
+# rip off last "/"
+ip=${SETUP_INSTALLPATH/%\//}
+
+# replace all "/" with "\/" to use with regexp.
+fn2=${ip//\//\\\/}
+
+ed -s $fn <<EOF
+,s/@prefix@/$fn2/g
+,wq
+EOF
+
