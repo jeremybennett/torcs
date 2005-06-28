2cea9722b55506697f49edd798e2282161680f4e
Bernhard Wymann <berni4you@gmx.ch>
2005-06-28 07:05:21 +0000

    Fixed realloc in WIN32 code path such that it works like described on the man page. Replaced pointer +4 with +sizeof(int) to avoid potential 64bit Windows trouble in the future.

--- src/libs/tgf/tgf.cpp
+++ src/libs/tgf/tgf.cpp
@@ -2,9 +2,9 @@
                           tgf.cpp -- The Gaming Framework                            
                              -------------------                                         
     created              : Fri Aug 13 22:31:43 CEST 1999
-    copyright            : (C) 1999 by Eric Espie                         
-    email                : torcs@free.fr   
-    version              : $Id$                                  
+    copyright            : (C) 1999 by Eric Espie
+    email                : torcs@free.fr
+    version              : $Id$
  ***************************************************************************/
 
 /***************************************************************************
@@ -19,6 +19,7 @@
 #ifdef WIN32
 #include <windows.h>
 #endif
+
 #include <tgf.h>
 #include <time.h>
 
@@ -28,158 +29,167 @@ extern void gfOsInit(void);
 extern void gfParamInit(void);
 extern void gfRlstInit(void);
 
-// <esppat>
+
 #ifdef WIN32
 #include <crtdbg.h>
 #include <assert.h>
-void *
-_tgf_win_malloc(size_t size)
+
+
+void * _tgf_win_malloc(size_t size)
 {
 #ifdef _DEBUG
- char * p = (char*)GlobalAlloc(GMEM_FIXED, size + 12 );
+	char * p = (char*)GlobalAlloc(GMEM_FIXED, size + 3*sizeof(int));
+	*(int*)(p) = size + 3*sizeof(int);
+	*((int*)p + 1) = 123456789;
+	*((int*)(p + size + 3*sizeof(int)) - 1) = 987654321;
 
- *(int*)( p ) = size + 12;
- *( (int*)p + 1 ) = 123456789;
- *( (int*)( p + size + 12 ) - 1 ) = 987654321;
-
- return p + 8;
+	return p + 2*sizeof(int);
 #else // _DEBUG
- char * p = (char*)GlobalAlloc(GMEM_FIXED, size + 4 );
-
- *(int*)( p ) = size;
-
- return p + 4;
+	char * p = (char*)GlobalAlloc(GMEM_FIXED, size + sizeof(int));
+	if (p == NULL) {
+		return NULL;
+	}
+	*(int*)(p) = size;
+	return p + sizeof(int);
 #endif // _DEBUG
 }
 
-void *
-_tgf_win_calloc(size_t num, size_t size)
+
+void * _tgf_win_calloc(size_t num, size_t size)
 {
- void * p = _tgf_win_malloc(num * size);
- memset(p, 0, num * size);
- return p;
+	void * p = _tgf_win_malloc(num * size);
+	memset(p, 0, num * size);
+	return p;
 }
 
-void *
-_tgf_win_realloc(void * memblock, size_t size)
+
+void * _tgf_win_realloc(void * memblock, size_t size)
 {
- void * p = _tgf_win_malloc(size);
+	if (size == 0) {
+		_tgf_win_free(memblock);
+		return NULL;
+	}
 
- if (!p) {
-  _tgf_win_free(memblock);
+	void * p = _tgf_win_malloc(size);
+	if (p == NULL) {
+		return NULL;
+	}
 
-  return NULL;
- }
- else {
-  memcpy(p, memblock, min(*(int*)((char*)memblock-4), (int)size) );
-  
-  return p;
- }
+	if (memblock != NULL) {
+		memcpy(p, memblock, min(*(int*)((char*)memblock-sizeof(int)), (int)size));
+		_tgf_win_free(memblock);
+	}
+	return p;
 }
 
-void
-_tgf_win_free(void * memblock)
+
+void _tgf_win_free(void * memblock)
 {
- if (!memblock)
-  return;
+	if (!memblock) {
+		return;
+	}
 
 #ifdef _DEBUG
- char * p = (char*)memblock - 8;
+	char * p = (char*)memblock - 2*sizeof(int);
 
- if ( !_CrtIsValidPointer( p, 4, TRUE ) )
-  assert( 0 );
+	if (!_CrtIsValidPointer(p, sizeof(int), TRUE)) {
+		assert(0);
+	}
 
- if ( !_CrtIsValidPointer( p, *(int*)p, TRUE ) )
-  assert( 0 );
+	if (!_CrtIsValidPointer(p, *(int*)p, TRUE)) {
+		assert( 0 );
+	}
 
- if ( *( (int*)p + 1 ) != 123456789 )
-  assert( 0 );
+	if (*((int*)p + 1) != 123456789) {
+		assert( 0 );
+	}
 
- if( *( (int*)( p + *(int*)p ) - 1 ) != 987654321 )
-  assert( 0 );
+	if(*((int*)(p + *(int*)p ) - 1) != 987654321) {
+		assert( 0 );
+	}
 
- GlobalFree((char*)memblock - 8);
+	GlobalFree((char*)memblock - 2*sizeof(int));
 #else // _DEBUG
- GlobalFree((char*)memblock - 4);
+	GlobalFree((char*)memblock - sizeof(int));
 #endif // _DEBUG
 }
 
-char *
-_tgf_win_strdup(const char * str)
-{
- char * s = (char*)_tgf_win_malloc(strlen(str)+1);
- strcpy(s,str);
 
- return s;
+char * _tgf_win_strdup(const char * str)
+{
+	char * s = (char*)_tgf_win_malloc(strlen(str)+1);
+	strcpy(s,str);
+
+	return s;
 }
 #endif // WIN32
-// </esppat>
 
 
-void
-GfInit(void)
+void GfInit(void)
 {
-    gfDirInit();
-    gfModInit();
-    gfOsInit();
-    gfParamInit();
+	gfDirInit();
+	gfModInit();
+	gfOsInit();
+	gfParamInit();
 }
 
-void
-gfMeanReset(tdble v, tMeanVal *pvt)
-{
-    int		i;
 
-    for (i = 0; i < GF_MEAN_MAX_VAL; i++) {
-	pvt->val[i] = v;
-    }
-}
-
-tdble
-gfMean(tdble v, tMeanVal *pvt, int n, int w)
+void gfMeanReset(tdble v, tMeanVal *pvt)
 {
-    int		i;
-    tdble	sum;
-    
-    if (n > pvt->curNum) {
-	if (pvt->curNum < GF_MEAN_MAX_VAL) {
-	    pvt->curNum++;
+	int i;
+
+	for (i = 0; i < GF_MEAN_MAX_VAL; i++) {
+		pvt->val[i] = v;
 	}
-	n = pvt->curNum;
-    } else {
-	pvt->curNum = n;
-    }
-    sum = 0;
-    for (i = 0; i < n; i++) {
-	pvt->val[i] = pvt->val[i + 1];
-	sum += pvt->val[i];
-    }
-    pvt->val[n] = v;
-    sum += (tdble)w * v;
-    sum /= (tdble)(n + w);
-
-    return sum;
 }
+
+
+tdble gfMean(tdble v, tMeanVal *pvt, int n, int w)
+{
+	int i;
+	tdble sum;
+
+	if (n > pvt->curNum) {
+		if (pvt->curNum < GF_MEAN_MAX_VAL) {
+			pvt->curNum++;
+		}
+		n = pvt->curNum;
+	} else {
+		pvt->curNum = n;
+	}
+
+	sum = 0;
+	for (i = 0; i < n; i++) {
+		pvt->val[i] = pvt->val[i + 1];
+		sum += pvt->val[i];
+	}
+
+	pvt->val[n] = v;
+	sum += (tdble)w * v;
+	sum /= (tdble)(n + w);
+
+	return sum;
+}
+
 
 static char bufstr[1024];
 
-char *
-GfGetTimeStr(void)
+char * GfGetTimeStr(void)
 {
-    struct tm		*stm;
-    time_t		t;
+	struct tm *stm;
+	time_t t;
 
-    t = time(NULL);
-    stm = localtime(&t);
-    sprintf(bufstr, "%4d%02d%02d%02d%02d%02d",
-	    stm->tm_year+1900,
-	    stm->tm_mon+1,
-	    stm->tm_mday,
-	    stm->tm_hour,
-	    stm->tm_min,
-	    stm->tm_sec);
+	t = time(NULL);
+	stm = localtime(&t);
+	sprintf(bufstr, "%4d%02d%02d%02d%02d%02d",
+		stm->tm_year+1900,
+		stm->tm_mon+1,
+		stm->tm_mday,
+		stm->tm_hour,
+		stm->tm_min,
+		stm->tm_sec);
 
-    return bufstr;
+	return bufstr;
 }
 
 
@@ -190,37 +200,38 @@ GfGetTimeStr(void)
     @return	Time string.
     @warning	The returned string has to be free by the caller.
  */
-char * 
-GfTime2Str(tdble sec, int sgn)
+char * GfTime2Str(tdble sec, int sgn)
 {
-    char  buf[256];
-    char* sign;
+	char buf[256];
+	char* sign;
 
-    if (sec < 0.0) {
-	sec = -sec;
-	sign = "-";
-    } else {
-	if (sgn) {
-	    sign = "+";
+	if (sec < 0.0) {
+		sec = -sec;
+		sign = "-";
 	} else {
-	    sign = "  ";
+		if (sgn) {
+			sign = "+";
+		} else {
+			sign = "  ";
+		}
 	}
-    }
-    int h = (int)(sec / 3600.0);
-    sec -= 3600 * h;
-    int m = (int)(sec / 60.0);
-    sec -= 60 * m;
-    int s = (int)(sec);
-    sec -= s;
-    int c = (int)floor((sec) * 100.0);
-    if (h) {
-	(void)sprintf(buf, "%s%2.2d:%2.2d:%2.2d:%2.2d", sign,h,m,s,c);
-    } else if (m) {
-	(void)sprintf(buf, "   %s%2.2d:%2.2d:%2.2d", sign,m,s,c);
-    } else {
-	(void)sprintf(buf, "      %s%2.2d:%2.2d", sign,s,c);
-    }
-    return strdup(buf);
+
+	int h = (int)(sec / 3600.0);
+	sec -= 3600 * h;
+	int m = (int)(sec / 60.0);
+	sec -= 60 * m;
+	int s = (int)(sec);
+	sec -= s;
+	int c = (int)floor((sec) * 100.0);
+
+	if (h) {
+		(void)sprintf(buf, "%s%2.2d:%2.2d:%2.2d:%2.2d", sign,h,m,s,c);
+	} else if (m) {
+		(void)sprintf(buf, "   %s%2.2d:%2.2d:%2.2d", sign,m,s,c);
+	} else {
+		(void)sprintf(buf, "      %s%2.2d:%2.2d", sign,s,c);
+	}
+	return strdup(buf);
 }
 
 
@@ -228,73 +239,74 @@ static char *localDir = "";
 static char *libDir = "";
 static char *dataDir = "";
 
-char *
-GetLocalDir(void)
+
+char * GetLocalDir(void)
 {
-    return localDir;
+	return localDir;
 }
 
-void
-SetLocalDir(char *buf)
+
+void SetLocalDir(char *buf)
 {
-    localDir = strdup(buf);
+	localDir = strdup(buf);
 }
 
-char *
-GetLibDir(void)
+
+char * GetLibDir(void)
 {
-    return libDir;
+	return libDir;
 }
 
-void
-SetLibDir(char *buf)
+
+void SetLibDir(char *buf)
 {
-    libDir = strdup(buf);
+	libDir = strdup(buf);
 }
 
-char *
-GetDataDir(void)
+
+char * GetDataDir(void)
 {
-    return dataDir;
+	return dataDir;
 }
 
-void
-SetDataDir(char *buf)
-{
-    dataDir = strdup(buf);
-}
 
+void SetDataDir(char *buf)
+{
+	dataDir = strdup(buf);
+}
 
 
 static int singleTextureMode = 0;
 
-int
-GetSingleTextureMode (void)
+
+int GetSingleTextureMode (void)
 {
-    return singleTextureMode;
+	return singleTextureMode;
 }
 
-void
-SetSingleTextureMode (void)
-{   
-    singleTextureMode = 1;
-}
 
-int
-GfNearestPow2 (int x)
+void SetSingleTextureMode (void)
 {
-    int r;
-
-    if (!x) return 0;
-
-    x++;
-    r = 1;
-    while ((1 << r) < x) {
-	r++;
-    }
-    r--;
-
-    return (1 << r);
+	singleTextureMode = 1;
 }
 
-	
+
+int GfNearestPow2 (int x)
+{
+	int r;
+
+	if (!x) {
+		return 0;
+	}
+
+	x++;
+	r = 1;
+	while ((1 << r) < x) {
+		r++;
+	}
+	r--;
+
+	return (1 << r);
+}
+
+
