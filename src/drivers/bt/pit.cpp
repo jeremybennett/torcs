5d57da929fcaee8f2d1aafa1eb7bf5199b67d0d3
Bernhard Wymann <berni4you@gmx.ch>
2006-04-27 22:37:34 +0000

    Fixed a few vc++ compiler warnings. Fixed missing include path in release project (need to put it into debug as well).

--- src/drivers/bt/pit.cpp
+++ src/drivers/bt/pit.cpp
@@ -165,7 +165,7 @@ bool Pit::isTimeout(float distance)
 		pittimer = 0.0f;
 		return false;
 	} else {
-		pittimer += RCM_MAX_DT_ROBOTS;
+		pittimer += (float) RCM_MAX_DT_ROBOTS;
 		if (pittimer > 3.0f) {
 			pittimer = 0.0f;
 			return true;
