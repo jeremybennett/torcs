06a32cc19b184b5c833382f89da43319cb58dc6f
Bernhard Wymann <berni4you@gmx.ch>
2008-11-23 13:50:12 +0000

    Version number to 1.3.1, fixed #1674545.

--- config.h
+++ config.h
@@ -128,7 +128,7 @@
 #define TIME_WITH_SYS_TIME 1
 
 /* Version number of package */
-#define VERSION "1.3.1-test1"
+#define VERSION "1.3.1"
 
 /* Define to 1 if your processor stores words with the most significant byte
    first (like Motorola and SPARC, unlike Intel and VAX). */
