cbd2634bb6b54ee3393a587e652563efcabdb577
Bernhard Wymann <berni4you@gmx.ch>
2011-12-28 15:11:57 +0000

    Menu music code

---
+++ src/libs/musicplayer/SoundStream.cpp
@@ -0,0 +1,22 @@
+/***************************************************************************
+
+    file                 : SoundStream.cpp
+    created              : Fri Dec 23 17:35:18 CET 2011
+    copyright            : (C) 2011 Bernhard Wymann
+    email                : berniw@bluewin.ch
+    version              : $Id$
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+/* Intherface for sound streams */
+
+#include "SoundStream.h"
