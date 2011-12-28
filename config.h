5534ab6d25d09aeb282a6cf88418638206228769
Bernhard Wymann <berni4you@gmx.ch>
2011-12-28 15:08:26 +0000

    Menu music code, version number 1.3.2-test2

--- config.h
+++ config.h
@@ -131,7 +131,7 @@
 #define TIME_WITH_SYS_TIME 1
 
 /* Version number of package */
-#define VERSION "1.3.2-test1"
+#define VERSION "1.3.2-test2"
 
 /* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
    significant byte first (like Motorola and SPARC, unlike Intel). */
