be04dd2a21a3755d5eb7a1767602c0f3b8fa3cc2
Bernhard Wymann <berni4you@gmx.ch>
2011-12-29 09:22:14 +0000

    Added 1280x720 and 1600x900 resolutions

--- src/libs/tgfclient/screen.cpp
+++ src/libs/tgfclient/screen.cpp
@@ -74,7 +74,7 @@ static int usedFG = 0;
 static char	**Res = NULL;
 static int nbRes = 0;
 #else // USE_RANDR_EXT
-static char	*Res[] = {"640x480", "800x600", "1024x768", "1152x768", "1152x864", "1200x854", "1200x960", "1280x1024", "1400x900", "1600x1200", "1680x1050", "1920x1200", "320x200"};
+static char	*Res[] = {"640x480", "800x600", "1024x768", "1152x768", "1152x864", "1200x854", "1200x960", "1280x720", "1280x1024", "1400x900", "1600x900", "1600x1200", "1680x1050", "1920x1200", "320x200"};
 static const int nbRes = sizeof(Res) / sizeof(Res[0]);
 #endif // USE_RANDR_EXT
 
