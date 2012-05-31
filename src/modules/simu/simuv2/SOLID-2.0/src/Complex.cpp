9424f549debfabf1f23f9479d007a07aed622576
Bernhard Wymann <berni4you@gmx.ch>
2012-05-31 23:19:22 +0000

    Fixed memory leak

--- src/modules/simu/simuv2/SOLID-2.0/src/Complex.cpp
+++ src/modules/simu/simuv2/SOLID-2.0/src/Complex.cpp
@@ -40,7 +40,7 @@ Complex::~Complex() {
   if (count >= 2) delete [] root;
   for (int i = 0; i < count; ++i) delete leaves[i].poly;
   delete [] leaves;
-  /* if (free_base) delete [] (void *)base.getPointer(); */  /* warning: deleting `void*' is undefined */
+  if (free_base) delete [] base.getPointer();
 }
 
 BBox Complex::bbox(const Transform& t) const {
