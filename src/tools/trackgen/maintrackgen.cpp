37b75935579c3181bcc96aca4b4a08f7919247a4
Bernhard Wymann <berni4you@gmx.ch>
2012-09-05 16:40:45 +0000

    *** empty log message ***

--- src/tools/trackgen/maintrackgen.cpp
+++ src/tools/trackgen/maintrackgen.cpp
@@ -136,7 +136,7 @@ void init_args(int argc, char **argv)
 	    {"version", 1, 0, 0}
 	};
 
-	c = getopt_long(argc, argv, "hvn:c:azsSE:H:bBL:",
+	c = getopt_long(argc, argv, "hvn:c:azsSE:H:rbBL:",
 			long_options, &option_index);
 	if (c == -1)
 	    break;
