bc688538b303cc38e00b7e05a16abcdce9ad2386
Bernhard Wymann <berni4you@gmx.ch>
2013-08-27 16:31:38 +0000

    Ensure original min/max parameter for pit setup adjustments

--- src/libs/raceengineclient/raceengine.cpp
+++ src/libs/raceengineclient/raceengine.cpp
@@ -58,6 +58,8 @@ ReUpdtPitTime(tCarElt *car)
 	switch (car->_pitStopType) {
 		case RM_PIT_REPAIR:
 			info->totalPitTime = 2.0f + fabs((double)(car->_pitFuel)) / 8.0f + (tdble)(fabs((double)(car->_pitRepair))) * 0.007f;
+			// Ensure that the right min/max values are in the setup structure (could have been modified by the robot))
+			RtInitCarPitSetup(car->_carHandle, &(car->pitcmd.setup), true);
 			car->_scheduledEventTime = s->currentTime + info->totalPitTime;
 			ReInfo->_reSimItf.reconfig(car);
 			for (i=0; i<4; i++) {
