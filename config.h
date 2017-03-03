e8d48c636790f67cf2d4663c5daae689ed69eed0
Bernhard Wymann <berni4you@gmx.ch>
2017-03-03 19:48:24 +0000

    Updated version to 1.3.8-test1

--- config.h
+++ config.h
@@ -134,7 +134,7 @@
 #define TIME_WITH_SYS_TIME 1
 
 /* Version number of package */
-#define VERSION "1.3.7"
+#define VERSION "1.3.8-test1"
 
 /* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
    significant byte first (like Motorola and SPARC, unlike Intel). */
